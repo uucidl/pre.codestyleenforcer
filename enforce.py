b15f403df564ec89f8e7bf36880ef07b9124947f
Nicolas Léveillé <nicolas@uucidl.com>
2013-06-18 09:26:26 +0200

    first functional enforcer
    this one implements a strong aversion to having globals mixed.
    
    Certain things which are tolerated in C such as having synonyms:
    
          struct a
          void a()
          int a
    
    will not be tolerated.
    
    The idea is to help out when you only have grep and sed to refactor.

--- enforce.py
+++ enforce.py
@@ -14,9 +14,11 @@ logger = logging.getLogger(__name__)
 def is_local_decl_kind(node_kind):
     return (
         node_kind.is_declaration() and
-        node_kind == clang.cindex.CursorKind.VAR_DECL and
-        node_kind == clang.cindex.CursorKind.PARM_DECL and
-        node_kind == clang.cindex.CursorKind.FIELD_DECL
+        (
+            node_kind == clang.cindex.CursorKind.VAR_DECL or
+            node_kind == clang.cindex.CursorKind.PARM_DECL or
+            node_kind == clang.cindex.CursorKind.FIELD_DECL
+        )
     )
 
 
@@ -30,7 +32,20 @@ def is_name_decl_kind(node_kind):
     )
 
 
-def find_globals(node, all_global_decls):
+def node_error_msg(node):
+    return '%s:%d: Error with symbol %s' % (node.location.file, node.location.line, node.spelling)
+
+
+def find_and_check_globals(node, all_global_decls, reject_node_predicate, output):
+    """
+    node a clang cursor
+    reject_node_predicate(node)
+    output(level, msg)
+    """
+
+    if reject_node_predicate(node):
+        return
+
     if node.kind.is_declaration():
         if not is_local_decl_kind(node.kind):
             if "" == node.spelling:
@@ -40,24 +55,48 @@ def find_globals(node, all_global_decls):
             is_proper_name = re.match(r"[A-Z]", '%s' % node.spelling)
 
             if is_name_decl_kind(node.kind) and not is_proper_name:
-                print '%s needs to start with a capital' % node.spelling
+                output(1, '%s should to start with a capital' %
+                       node_error_msg(node))
 
             if not is_name_decl_kind(node.kind) and is_proper_name:
-                print '%s needs to start with lower case' % node.spelling
+                output(1, '%s should start with lower case' %
+                       node_error_msg(node))
 
-            all_global_decls.append(node.spelling)
+            if (node.spelling in all_global_decls
+                    and node.kind != all_global_decls[node.spelling]):
+                output(0, '%s should not appear multiple times (kind: %s, other kind: %s)' % (
+                    node_error_msg(node),
+                    node.kind,
+                    all_global_decls[node.spelling]
+                ))
+
+            all_global_decls[node.spelling] = node.kind
 
     if node.kind == clang.cindex.CursorKind.INCLUSION_DIRECTIVE:
         return
 
     for n in node.get_children():
-        find_globals(n, all_global_decls)
+        find_and_check_globals(
+            n, all_global_decls, reject_node_predicate, output)
 
 
-def check_locals(node, all_global_decls):
+def check_locals(node, all_global_decls, reject_node_predicate, output):
+    """
+    node a clang cursor
+    reject_node_predicate(node)
+    output(level, msg)
+    """
+
+    if reject_node_predicate(node):
+        return
+
     if is_local_decl_kind(node.kind):
         if node.spelling in all_global_decls:
-            print '%s is duplicated' % node.spelling
+            output(0, '%s is also a global symbol (kind: %s, other kind: %s)' % (
+                node_error_msg(node),
+                node.kind,
+                all_global_decls[node.spelling]
+            ))
 
 if __name__ == "__main__":
     logging.basicConfig()
@@ -69,7 +108,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check a codebase")
     parser.add_argument('directory', type=directory, nargs='+')
     parser.add_argument('--verbose', dest='is_verbose', action='store_true')
-
+    parser.add_argument('--level', dest='max_level', type=int, default=0)
     args = parser.parse_args()
 
     if args.is_verbose:
@@ -85,14 +124,32 @@ if __name__ == "__main__":
                 if f.endswith(".h") or f.endswith(".c"):
                     all_files.append(os.path.join(root, f))
 
-    all_global_decls = []
+    all_global_decls = {}
+
+    def reject_nodes_outside(node):
+        if node.kind == clang.cindex.CursorKind.TRANSLATION_UNIT:
+            return False
+
+        return (
+            not node.location.file or
+            not any(
+                os.path.abspath(node.location.file.name).startswith(
+                    os.path.abspath(root))
+                for root in args.directory
+            )
+        )
+
+    def output(level, msg):
+        if level <= args.max_level:
+            print msg
 
     for fn in all_files:
         logger.info("parsing %s" % fn)
         translation_unit = index.parse(fn)
         logger.info("translation unit %s" % translation_unit.spelling)
 
-        find_globals(translation_unit.cursor, all_global_decls)
+        find_and_check_globals(
+            translation_unit.cursor, all_global_decls, reject_nodes_outside, output)
 
     all_global_decls = frozenset(all_global_decls)
 
@@ -101,4 +158,5 @@ if __name__ == "__main__":
         translation_unit = index.parse(fn)
         logger.info("translation unit %s" % translation_unit.spelling)
 
-        check_locals(translation_unit.cursor, all_global_decls)
+        check_locals(translation_unit.cursor,
+                     all_global_decls, reject_nodes_outside, output)
